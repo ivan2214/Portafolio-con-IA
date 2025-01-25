6b9b138a5ddabe1d486740fcb35512a5ced4dc8c
ivan2214 <bongiovanniivan12@gmail.com>
2025-01-25 17:57:12 -0300

    feat: agregar íconos de redes sociales y accesibilidad

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,23 +1,29 @@
 import type { NavLink, SocialLink } from "@/types";
+import { MdiGithub } from "./components/icons/github";
+import { MdiLinkedin } from "./components/icons/linkedin";
+import { MdiTwitter } from "./components/icons/twitter";
 
 export const NAV_LINKS: NavLink[] = [
-	{
-		title: "Repositorios",
-		href: "/repositories",
-	},
+  {
+    title: "Repositorios",
+    href: "/repositories",
+  },
 ];
 
 export const SOCIAL_LINKS: SocialLink[] = [
-	{
-		name: "GitHub",
-		href: "https://github.com/ivan2214",
-	},
-	{
-		name: "LinkedIn",
-		href: "https://linkedin.com/in/bongiovanni-ivan45/",
-	},
-	{
-		name: "Twitter",
-		href: "https://twitter.com/bongiovanniDev",
-	},
+  {
+    name: "GitHub",
+    href: "https://github.com/ivan2214",
+    icon: MdiGithub,
+  },
+  {
+    name: "LinkedIn",
+    href: "https://linkedin.com/in/bongiovanni-ivan45/",
+    icon: MdiLinkedin,
+  },
+  {
+    name: "Twitter",
+    href: "https://twitter.com/bongiovanniDev",
+    icon: MdiTwitter,
+  },
 ];
